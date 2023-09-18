843d27d86b4255658c27e36d8f46677ae30aa156
Swarup Saw <bittu28swarup@gmail.com>
2023-09-18 15:28:58 +0530

    Update app.js

--- pokeprompt/app.js
+++ pokeprompt/app.js
@@ -80,10 +80,10 @@ function submitPrompt(prompt = "hi") {
         "User_json": {
             "username": metadata.User_json.username,
             "member": metadata.User_json.member,
-            "total_score": 12.0,
+            "total_score": 0.0,
             "level": {
                 'level_1': {
-                    "score": 12.0,
+                    "score": 0.0,
                     "start_time": "HH:MM:SS DD:MM:YYYY",
                 }
             }
@@ -95,10 +95,10 @@ function submitPrompt(prompt = "hi") {
         "User_json": {
             "username": metadata.User_json.username,
             "member": metadata.User_json.member,
-            "total_score": 12.0,
+            "total_score": 0.0,
             "level": {
                 'level_2': {
-                    "score": 12.0,
+                    "score": 0.0,
                     "start_time": "HH:MM:SS DD:MM:YYYY",
                 }
             }
@@ -110,10 +110,10 @@ function submitPrompt(prompt = "hi") {
         "User_json": {
             "username": metadata.User_json.username,
             "member": metadata.User_json.member,
-            "total_score": 12.0,
+            "total_score": 0.0,
             "level": {
                 'level_3': {
-                    "score": 12.0,
+                    "score": 0.0,
                     "start_time": "HH:MM:SS DD:MM:YYYY",
                 }
             }
@@ -162,22 +162,6 @@ function submitPrompt(prompt = "hi") {
 
     if (level == 1) {
         xhr.send(JSON.stringify(data1));
-        document.getElementById('dialog-you-win').showModal();
-        if (xhr.status === 200) {
-            var apiResponse = JSON.parse(xhr.responseText);
-            var user = apiResponse.res.find(function(item) {
-                return item.userID === userID;
-            });
-
-            if (user) {
-                callback(user.score);
-            } else {
-                callback(null); // User not found
-            }
-        } else {
-            console.error("Request failed with status:", xhr.status);
-            callback(null); // Request failed
-        }
 
     } else if (level == 2) {
         xhr.send(JSON.stringify(data2));
